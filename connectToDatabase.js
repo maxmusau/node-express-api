04cf5a7cbe9ce0c6296949a3d014c9ac6a57a9bb
Musau_Maxwell <musaumaxwell@gmail.com>
2024-05-13 11:25:43 +0300

    Update connectToDatabase.js

--- connectToDatabase.js
+++ connectToDatabase.js
@@ -4,7 +4,7 @@ const { MongoClient, Server } = require("mongodb");
 // connect to the db
 const url = process.env.DATABASE_URL;
 // const cert = process.env.CA_CERT;
-const dbName = "tweets";
+const dbName = "admin";
 
 if (!url) throw new Error("Please enter a DATABASE_URL");
 // if (!cert) throw new Error("Please enter a CA_CERT");
@@ -20,7 +20,7 @@ module.exports = async function connectToDatabase() {
   }
 
   const client = await MongoClient.connect(url);
-  const db = client.db(dbName);
+  const db = client.db(admin);
 
   cachedClient = client;
   cachedDb = db;
